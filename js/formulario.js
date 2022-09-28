03526acd7b4a9f220a608fc95766b71f05e5507d
Bruno Kalebe <brunokalebe@gmail.com>
2022-09-28 15:34:53 -0300

    Arrumado um pequeno bug no texto de sucesso/falha

--- js/formulario.js
+++ js/formulario.js
@@ -52,9 +52,9 @@ formulario.addEventListener('submit', enviarFormulario)
 const formulario = document.querySelector('form');
 function formularioEnviado(resposta) {
   if (resposta.ok) {
-    formulario.innerHTML = "<p class='font-2-l style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #317a00'>Mensagem enviada</span>, em breve entraremos em contato. Geralmente respondemos em 24h.</p>"
+    formulario.innerHTML = "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7';><span style='color: #317a00'>Mensagem enviada</span>, em breve entraremos em contato. Geralmente respondemos em 24h.</p>"
   } else {
-    formulario.innerHTML = "<p class='font-2-l style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #E00000'>Erro no envio</span>, você pode enviar diretamente para o nosso email em: contato@bikcraftjs.hypegrowth.com.br</p>";
+    formulario.innerHTML = "<p class='font-2-l' style='background: #f7f7f7; grid-column: 1/-1; padding: 1rem; border-radius: 4px';><span style='color: #E00000'>Erro no envio</span>, você pode enviar diretamente para o nosso email em: contato@bikcraftjs.hypegrowth.com.br</p>";
   }
 }
 function enviarFormulario(event) {
